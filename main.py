d706f68537646893efff3548d070cf433329af45
thgribeiro <thiagordlr@gmail.com>
2022-09-12 15:57:15 -0300

    versao teste 1.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
 import telepot
@@ -11,14 +12,17 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+#navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+navegador = webdriver.Chrome()
 navegador.get('https://ssw.inf.br/2/rastreamento_pf?')
 navegador.find_element(By.XPATH, '//*[@id="cnpjdest"]').send_keys('08276822550')
 navegador.find_element(By.XPATH, '//*[@id="btn_rastrear"]').click()
 sleep(2)
+info = navegador.find_element(By.XPATH, '/html/body/div[5]/div[1]/div[3]/table[2]/tbody/tr[2]/td[3]/p[1]').text
+bot.sendMessage(1167845071, f'📦O status atual da sua encomenda é:\n\n{info}')
 
 while True:
+    sleep(60)
     navegador.refresh()
     info = navegador.find_element(By.XPATH, '/html/body/div[5]/div[1]/div[3]/table[2]/tbody/tr[2]/td[3]/p[1]').text
     bot.sendMessage(1167845071, f'📦O status atual da sua encomenda é:\n\n{info}')
-    navegador.implicitly_wait(7200)
