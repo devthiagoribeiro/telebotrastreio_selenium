0e835b3655eb25f4d14e46f0c6e78ef957874197
thgribeiro <thiagordlr@gmail.com>
2022-09-12 15:12:47 -0300

    alterações para upar na heroku

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import telepot
 import os
 
 bot = telepot.Bot('5641283166:AAEiLUp81ecbOuE28-OEWfntiVLjGVkwbsk')
-navegador = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 navegador.get('https://ssw.inf.br/2/rastreamento_pf?')
 navegador.find_element(By.XPATH, '//*[@id="cnpjdest"]').send_keys('08276822550')
 navegador.find_element(By.XPATH, '//*[@id="btn_rastrear"]').click()
