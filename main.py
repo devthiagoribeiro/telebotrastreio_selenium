417bb8cc1b2a37ee273c8b8a3a62f3b464d18a54
thgribeiro <thiagordlr@gmail.com>
2022-11-18 22:46:50 -0300

    update

--- main.py
+++ main.py
@@ -4,35 +4,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
 import telepot
+import chromedriver_autoinstaller
 import os
 
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 3000
-
-bot = telepot.Bot('5641283166:AAEiLUp81ecbOuE28-OEWfntiVLjGVkwbsk')
+channelid = os.environ.get('CHANNEL_ID')
+cpf = os.environ.get('CPF')
+bot = os.environ.get('BOT_TOKEN') #telepot.Bot('5641283166:AAEiLUp81ecbOuE28-OEWfntiVLjGVkwbsk')
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+# navegador = webdriver.Chrome(executable_path=chromedriver_autoinstaller.install())
 navegador.get('https://ssw.inf.br/2/rastreamento_pf?')
-navegador.find_element(By.XPATH, '//*[@id="cnpjdest"]').send_keys('08276822550')
+navegador.find_element(By.XPATH, '//*[@id="cnpjdest"]').send_keys(cpf)
 navegador.find_element(By.XPATH, '//*[@id="btn_rastrear"]').click()
 sleep(2)
 while True:
     navegador.refresh()
     info = navegador.find_element(By.XPATH, '/html/body/div[5]/div[1]/div[3]/table[2]/tbody/tr[2]/td[3]/p[1]').text
-    bot.sendMessage(1167845071, f'📦O status atual da sua encomenda é:\n\n{info}')
-    while True:
+    bot.sendMessage(channelid, f'📦O status atual da sua encomenda é:\n\n{info}')
+    new_info = info
+    while new_info == info:
         navegador.refresh()
         new_info = navegador.find_element(By.XPATH, '/html/body/div[5]/div[1]/div[3]/table[2]/tbody/tr[2]/td[3]/p[1]').text
-        if new_info != info:
-            info = navegador.find_element(By.XPATH, '/html/body/div[5]/div[1]/div[3]/table[2]/tbody/tr[2]/td[3]/p[1]').text
-            bot.sendMessage(1167845071, f'📦O status atual da sua encomenda é:\n\n{info}')
-        sleep(10)
+        sleep(240)
